2eee52c92c20cfb14ef04d950b52b8f95147ed46
Nurlan <ikhsan.nurlan@gmail.com>
2022-07-03 21:57:39 +0600

    simple web app

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -20,6 +21,56 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/bad", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}).Methods("GET")
+
+	router.HandleFunc("/name/{PARAM}", func(writer http.ResponseWriter, request *http.Request) {
+		vv, ok := mux.Vars(request)["PARAM"]
+		if !ok {
+			http.Error(writer, "empty param", http.StatusBadRequest)
+		}
+		data := fmt.Sprintf("Hello, %s!", vv)
+		fprintf, err := fmt.Fprintf(writer, data)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+		}
+		fmt.Printf("%d bytes were written\n", fprintf)
+
+	}).Methods("GET")
+
+	router.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+
+		fprintf, err := fmt.Fprintf(w, "I got message:\n%v", string(body))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		fmt.Printf("%d bytes were written\n", fprintf)
+	}).Methods("POST")
+
+	router.HandleFunc("/headers", func(w http.ResponseWriter, r *http.Request) {
+		a := r.Header.Get("a")
+		b := r.Header.Get("b")
+
+		aI, err := strconv.Atoi(a)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		bI, err := strconv.Atoi(b)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+
+		res := aI + bI
+
+		w.Header().Set("a+b", strconv.Itoa(res))
+
+	}).Methods("POST")
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
